7aa91df41a13f1f1b64735599d5eb43279170de0
Adhyatma Hawale <adhyatmahawale1008@gmail.com>
2025-09-07 22:12:12 +0530

    Added Routes in src/App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import { useState } from "react";
 import Alert from "./components/Alert";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default function App() {
   const [mode, setMode] = useState('light');
@@ -33,13 +38,16 @@ export default function App() {
 
   return (
     <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-      <Navbar title="TextUtils" mode={mode} aboutText="About" toggleMode={toggleMode}/>
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} aboutText="About" toggleMode={toggleMode}/>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
