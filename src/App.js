53d2db40d2c732775a59233b48b7732ff07869f3
Adhyatma Hawale <adhyatmahawale1008@gmail.com>
2025-09-07 22:42:48 +0530

    Removed routes and removed About component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import { useState } from "react";
 import Alert from "./components/Alert";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 export default function App() {
   const [mode, setMode] = useState('light');
@@ -38,16 +38,16 @@ export default function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" mode={mode} aboutText="About" toggleMode={toggleMode}/>
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
-          </Routes>
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />} /> */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}
+          {/* </Routes> */}
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
